4ae6539aa2c83f9fb8868364a390170df3a23aa4
Christin115 <cmjordan115@gmail.com>
2026-07-18 19:50:11 -0400

    tests passed

---
+++ client/script.js
@@ -0,0 +1,30 @@
+fetch("http://localhost:5000/events")
+  .then(response => response.json())
+  .then(events => {
+    events.forEach(renderEvent);
+  });
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector("#title").value;
+
+  fetch("http://localhost:5000/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ title })
+  })
+    .then(response => response.json())
+    .then(event => {
+      renderEvent(event);
+      document.querySelector("#title").value = "";
+    });
+});
+
+function renderEvent(event) {
+  const li = document.createElement("li");
+  li.textContent = event.title;
+  document.querySelector("#event-list").appendChild(li);
+}
